8b22a3783079c1383b1ea112cc0eb3b8e60f079a
Ishinaru <dvplima.cic@uesc.br>
2024-03-13 01:55:15 -0300

    criando aula-02

---
+++ java-avancado/aula-02/src/main/java/org/aula02/crudpostgresonetomany/Aula02Application.java
@@ -0,0 +1,13 @@
+package org.aula02.crudpostgresonetomany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aula02Application.class, args);
+    }
+
+}
---
+++ java-avancado/aula-02/src/test/java/org/aula02/crudpostgresonetomany/Aula02ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aula02.crudpostgresonetomany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
