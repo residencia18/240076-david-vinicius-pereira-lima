4cf8bcfcc5ac3e3128a969ebbfe5c71784fb4705
Ishinaru <dvplima.cic@uesc.br>
2024-03-22 16:13:49 -0300

    atualizando classe ProjectController com log

--- java-avancado/aula-05/src/main/java/com/loguse/aula05/Aula05Application.java
+++ java-avancado/aula-05/src/main/java/com/loguse/aula05/Aula05Application.java
@@ -1,13 +1,17 @@
 package com.loguse.aula05;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Aula05Application {
-
+	public static final Logger log = LoggerFactory.getLogger(Aula05Application.class);
 	public static void main(String[] args) {
 		SpringApplication.run(Aula05Application.class, args);
+		log.info("Application started...");
 	}
 
 }
--- java-avancado/aula-05/src/main/java/com/loguse/aula05/controller/ProjectController.java
+++ java-avancado/aula-05/src/main/java/com/loguse/aula05/controller/ProjectController.java
@@ -4,37 +4,40 @@ import com.loguse.aula05.module.Employee;
 import com.loguse.aula05.module.Project;
 import com.loguse.aula05.repository.EmployeeRepository;
 import com.loguse.aula05.repository.ProjectRepository;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("api/project")
 
 public class ProjectController {
+    private static Logger log = (Logger) LoggerFactory.getLogger(ProjectController.class);
     @Autowired
     private ProjectRepository projectRepository;
     @Autowired
     private EmployeeRepository employeeRepository;
     @PostMapping("/createProject")
     public String createProject(@RequestBody Project entity){
-        System.out.println("Create a new Project.");
+        log.info("Create a new Project.");
         Project project = new Project(entity.getProjectName(), entity.getTechnologyUsed());
         project = projectRepository.save(project);
-        System.out.println("Saved Project:\t"+project+"\n");
+        log.info("Saved Project:\t"+project+"\n");
         return "Project saved!!!";
     }
     @PostMapping("/createProjectForEmployees")
     public String createProjectForEmployees(@RequestBody Project entity){
-        System.out.println("Create a new Project and add existing Employees into this Project.");
+        log.info("Create a new Project and add existing Employees into this Project.");
         int empId = 1;
         Employee employee1 = employeeRepository.getReferenceById(empId);
-        System.out.println("Employee details:\t"+employee1.toString()+"\n");
+        log.info("Employee details:\t"+employee1.toString()+"\n");
         empId = 2;
         Employee employee2 = employeeRepository.getReferenceById(empId);
-        System.out.println("Employee details:\t"+employee2.toString()+"\n");
+        log.info("Employee details:\t"+employee2.toString()+"\n");
 
         Project project = new Project(entity.getProjectName(), entity.getTechnologyUsed());
         Set<Employee> employees = new HashSet<>();
@@ -43,22 +46,22 @@ public class ProjectController {
 
         project.setEmployees(employees);
         project = projectRepository.save(project);
-        System.out.println("\nSaved Project :: " + project + "\n");
+        log.info("\nSaved Project :: " + project + "\n");
 
         return "Project saved!!!";
     }
 
     @PostMapping("/assignProjectToEmployees/{projId}/{empId}")
     public String assignProjectToEmployees(@PathVariable(name = "projId") Integer projId, @PathVariable(name = "empId") Integer empId){
-        System.out.println("\nFetch existing Project and add existing Employee into this Project." + "\n");
+        log.info("\nFetch existing Project and add existing Employee into this Project." + "\n");
 
         // get Employee
         Employee employee = this.employeeRepository.getById(empId);
-        System.out.println("\nEmployee details :: " + employee.toString() + "\n");
+        log.info("\nEmployee details :: " + employee.toString() + "\n");
 
         // new Project
         Project project = this.projectRepository.getById(projId);
-        System.out.println("\nProject details :: " + project.toString() + "\n");
+        log.info("\nProject details :: " + project.toString() + "\n");
 
         // create Employee set
         Set<Employee> employees = new HashSet<>();
@@ -69,21 +72,21 @@ public class ProjectController {
 
         // save Project
         project = projectRepository.save(project);
-        System.out.println("\nSaved Project :: " + project + "\n");
+        log.info("\nSaved Project :: " + project + "\n");
 
         return "Project saved!!!";
     }
 
     @GetMapping(value = "/getProject/{projId}")
     public String getProject(@PathVariable(name = "projId") Integer projId) {
-        System.out.println("Fetch Project and its Employees." + "\n");
+        log.info("Fetch Project and its Employees." + "\n");
 
         // get Project details
         Project project = this.projectRepository.getById(projId);
-        System.out.println("\nProject details :: " + project.toString() + "\n");
-        System.out.println("\nEmployees details :: " + project.getEmployees() + "\n");
+        log.info("\nProject details :: " + project.toString() + "\n");
+        log.info("\nEmployees details :: " + project.getEmployees() + "\n");
 
-        System.out.println("Done!!!\n");
+        log.info("Done!!!\n");
 
         return "Project fetched successfully!!!";
     }
