029e9b9f275a3a5f099298a28196855a970e5dee
Ishinaru <dvplima.cic@uesc.br>
2024-03-24 11:21:23 -0300

    adicionando lombok ao pom.xml

---
+++ java-avancado/aula-06/src/test/java/org/aula06/lombok/Aula06ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aula06.lombok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula06ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
