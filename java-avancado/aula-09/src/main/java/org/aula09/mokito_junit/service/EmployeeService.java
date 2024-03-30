838ed414e062fcc98085d14e277627cdcf75253b
Ishinaru <dvplima.cic@uesc.br>
2024-03-30 12:57:15 -0300

    adding class EmployeeService and implementing

---
+++ java-avancado/aula-09/src/main/java/org/aula09/mokito_junit/service/EmployeeService.java
@@ -0,0 +1,27 @@
+package org.aula09.mokito_junit.service;
+
+import org.aula09.mokito_junit.module.Employee;
+import org.aula09.mokito_junit.repository.EmployeeRepository;
+
+import java.util.Optional;
+
+public class EmployeeService {
+    private EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public Employee create(Employee employee){
+        return this.employeeRepository.save(employee);
+    }
+
+    public Optional<Employee> update(Integer employeeId, Employee updatedEmployee){
+        Optional<Employee> existingEmployee = employeeRepository.findById(employeeId);
+        if(existingEmployee.isPresent()){
+            updatedEmployee.setId(employeeId);
+            return Optional.of(employeeRepository.save(updatedEmployee));
+        }
+        return Optional.empty();
+    }
+}
---
+++ java-avancado/aula-09/src/test/java/org/aula09/mokito_junit/service/EmployeeServiceTest.java
@@ -0,0 +1,8 @@
+package org.aula09.mokito_junit.service;
+
+import org.aula09.mokito_junit.repository.EmployeeRepository;
+import org.mockito.Mock;
+public class EmployeeServiceTest {
+    @Mock
+    private EmployeeRepository employeeRepository;
+}
